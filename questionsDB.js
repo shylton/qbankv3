f9170f25aa816e21b858ec10c7a5fc22fe1b1209
shylton <55864449+shylton@users.noreply.github.com>
2024-08-23 14:03:50 +1200

    Update questionsDB.js

--- questionsDB.js
+++ questionsDB.js
@@ -1,4 +1,4 @@
-export const questions = [
+export let questions = [
   {
     "prompt": "A 49-year-old male presents with recurrent purulent otorrhea and hearing loss. On exam there is retraction of the tympanic membrane with debris and a white mass visible behind the tympanic membrane. What is the most likely diagnosis?",
     "answers": [
